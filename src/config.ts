2175365e15b379b6b27b4a9843777bd02cba5a64
yunfan <jyf1987@gmail.com>
2026-04-29 11:36:35 +0800

    fix default value

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const NODE_LIGHTSTEP = Number(import.meta.env.VITE_NODE_LIGHTSTEP) || 0.08;
+export const NODE_LIGHTSTEP = Number(import.meta.env.VITE_NODE_LIGHTSTEP) || 0.15;
 export const NODE_LEVELMAX = Number(import.meta.env.VITE_NODE_LEVELMAX) || 5;
-export const NODE_HUESIZE = Number(import.meta.env.VITE_NODE_HUESIZE) || 60;
+export const NODE_HUESIZE = Number(import.meta.env.VITE_NODE_HUESIZE) || 40;
